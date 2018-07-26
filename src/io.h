7d6d7be0e0d651daee5f25b4f6f642a2af63b2de
Natsuki HOSONO <natsuki.hosono@riken.jp>
2018-07-26 16:49:42 +0900

    initial commit

---
+++ src/io.h
@@ -0,0 +1,73 @@
+#pragma once
+
+template <class ThisPtcl> void OutputBinary(PS::ParticleSystem<ThisPtcl>& sph_system, const system_t& sysinfo){
+	//Binary
+	char filename[256];
+	std::ofstream fout;
+	sprintf(filename, "result/%05d_%05d.bin", PS::Comm::getNumberOfProc(), PS::Comm::getRank());
+	fout.open(filename, std::ios::out | std::ios::binary | std::ios::trunc);
+	if(!fout){
+		std::cout << "cannot write restart file." << std::endl;
+		exit(1);
+	}
+	fout.write(reinterpret_cast<const char * const>(&sysinfo), sizeof(system_t));
+	for(std::size_t i = 0 ; i < sph_system.getNumberOfParticleLocal() ; ++ i){
+		ThisPtcl ith = sph_system[i];
+		fout.write((char*)&ith, sizeof(ThisPtcl));
+	}
+	fout.close();
+}
+
+template <class ThisPtcl> void OutputFileWithTimeInterval(PS::ParticleSystem<ThisPtcl>& sph_system, system_t& sysinfo, const PS::F64 end_time){
+	if(sysinfo.time > sysinfo.output_time){
+		FileHeader header;
+		header.time = sysinfo.time;
+		header.Nbody = sph_system.getNumberOfParticleLocal();
+		char filename[256];
+		sprintf(filename, "result/%05d", sysinfo.output_id);
+		sph_system.writeParticleAscii(filename, "%s_%05d_%05d.dat", header);
+		if(PS::Comm::getRank() == 0){
+			std::cout << "//================================" << std::endl;
+			std::cout << "output " << filename << "." << std::endl;
+			std::cout << "//================================" << std::endl;
+		}
+		sysinfo.output_time += end_time / PARAM::NUMBER_OF_SNAPSHOTS;
+		++ sysinfo.output_id;
+	}
+}
+
+template <class ThisPtcl> void InputFileWithTimeInterval(PS::ParticleSystem<ThisPtcl>& sph_system, system_t& sysinfo){
+	FileHeader header;
+	char filename[256];
+	sprintf(filename, "result/%05d", sysinfo.step);
+	sph_system.readParticleAscii(filename, "%s_%05d_%05d.dat", header);
+	sysinfo.time = header.time;
+	std::cout << header.time << std::endl;
+}
+
+template <class ThisPtcl> void InputBinary(PS::ParticleSystem<ThisPtcl>& sph_system, system_t& sysinfo){
+	char filename[256];
+	//sprintf(filename, "result/%05d_%05d_%05d.bin", sysinfo->step, PS::Comm::getNumberOfProc(), PS::Comm::getRank());
+	sprintf(filename, "./%05d_%05d.bin", PS::Comm::getNumberOfProc(), PS::Comm::getRank());
+	std::ifstream fin(filename, std::ios::in | std::ios::binary);
+	if(!fin){
+		std::cout << "cannot open restart file." << std::endl;
+		exit(1);
+	}
+	std::vector<ThisPtcl> ptcl;
+	fin.read((char*)&sysinfo, sizeof(system_t));
+	//sysinfo.step ++;
+	while(1){
+		ThisPtcl ith;
+		fin.read((char*)&ith, sizeof(ThisPtcl));
+		if(fin.eof() == true) break;
+		ptcl.push_back(ith);
+	}
+	fin.close();
+	sph_system.setNumberOfParticleLocal(ptcl.size());
+		for(std::size_t i = 0 ; i < ptcl.size() ; ++ i){
+		sph_system[i] = ptcl[i];
+	}
+	std::cout << "read binary" << std::endl;
+}
+
---
+++ src/main.cpp
@@ -0,0 +1,135 @@
+#include <particle_simulator.hpp>
+
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+
+#include "param.h"
+#include "mathfunc.h"
+#include "kernel.h"
+#include "EoS.h"
+#include "class.h"
+#include "init/GI.h"
+#include "force.h"
+#include "io.h"
+#include "integral.h"
+
+int main(int argc, char* argv[]){
+	namespace PTCL = STD;
+	typedef GI<PTCL::RealPtcl> PROBLEM;
+	//////////////////
+	//Create vars.
+	//////////////////
+	PS::Initialize(argc, argv);
+	PS::ParticleSystem<PTCL::RealPtcl> sph_system;
+	sph_system.initialize();
+	PS::DomainInfo dinfo;
+	dinfo.initialize();
+	system_t sysinfo;
+
+	sph_system.setAverageTargetNumberOfSampleParticlePerProcess(200);
+	//////////////////
+	//Setup Initial
+	//////////////////
+	if(argc == 1){
+		PROBLEM::setupIC(sph_system, sysinfo, dinfo);
+		PROBLEM::setEoS(sph_system);
+		PTCL::CalcPressure(sph_system);
+	}else{
+		sysinfo.step = atoi(argv[1]);
+		InputFileWithTimeInterval<PTCL::RealPtcl>(sph_system, sysinfo);
+		PROBLEM::setEoS(sph_system);
+	}
+
+	#pragma omp parallel for
+	for(PS::S32 i = 0 ; i < sph_system.getNumberOfParticleLocal() ; ++ i){
+		sph_system[i].initialize();
+	}
+	OutputFileWithTimeInterval(sph_system, sysinfo, PROBLEM::END_TIME);
+
+	//Dom. info
+	dinfo.decomposeDomainAll(sph_system);
+	sph_system.exchangeParticle(dinfo);
+	//plant tree
+	PS::TreeForForceShort<PTCL::RESULT::Dens , PTCL::EPI::Dens , PTCL::EPJ::Dens >::Gather   dens_tree;
+	PS::TreeForForceShort<PTCL::RESULT::Drvt , PTCL::EPI::Drvt , PTCL::EPJ::Drvt >::Gather   drvt_tree;
+	PS::TreeForForceShort<PTCL::RESULT::Hydro, PTCL::EPI::Hydro, PTCL::EPJ::Hydro>::Symmetry hydr_tree;
+	#ifdef SELF_GRAVITY
+	PS::TreeForForceLong <PTCL::RESULT::Grav , PTCL::EPI::Grav , PTCL::EPJ::Grav >::Monopole grav_tree;
+	#endif
+
+	dens_tree.initialize(sph_system.getNumberOfParticleLocal(), 0.5, 4, 128);
+	drvt_tree.initialize(sph_system.getNumberOfParticleLocal(), 0.5, 4, 128);
+	hydr_tree.initialize(sph_system.getNumberOfParticleLocal(), 0.5, 4, 128);
+	#ifdef SELF_GRAVITY
+	grav_tree.initialize(sph_system.getNumberOfParticleLocal(), 0.5, 8, 256);
+	#endif
+	for(short int loop = 0 ; loop <= PARAM::NUMBER_OF_DENSITY_SMOOTHING_LENGTH_LOOP ; ++ loop){
+		dens_tree.calcForceAllAndWriteBack(PTCL::CalcDensity(), sph_system, dinfo);
+	}
+
+	PTCL::CalcPressure(sph_system);
+	drvt_tree.calcForceAllAndWriteBack(PTCL::CalcDerivative(), sph_system, dinfo);
+	hydr_tree.calcForceAllAndWriteBack(PTCL::CalcHydroForce(), sph_system, dinfo);
+	#ifdef SELF_GRAVITY
+	grav_tree.calcForceAllAndWriteBack(PTCL::CalcGravityForce<PTCL::EPJ::Grav>(), PTCL::CalcGravityForce<PS::SPJMonopole>(), sph_system, dinfo);
+	#endif
+	sysinfo.dt = getTimeStepGlobal<PTCL::RealPtcl>(sph_system);
+	PROBLEM::addExternalForce(sph_system, sysinfo);
+	OutputFileWithTimeInterval(sph_system, sysinfo, PROBLEM::END_TIME);
+
+	if(PS::Comm::getRank() == 0){
+		std::cout << "//================================" << std::endl;
+		std::cout << std::scientific << std::setprecision(16) << "time = " << sysinfo.time << ", dt = " << sysinfo.dt << std::endl;
+		std::cout << "step = " << sysinfo.step << std::endl;
+		std::cout << "//================================" << std::endl;
+	}
+
+	while(sysinfo.time < PROBLEM::END_TIME){
+		#pragma omp parallel for
+		for(int i = 0 ; i < sph_system.getNumberOfParticleLocal() ; ++ i){
+			sph_system[i].initialKick(sysinfo.dt);
+			sph_system[i].fullDrift(sysinfo.dt);
+			sph_system[i].predict(sysinfo.dt);
+		}
+		sysinfo.time += sysinfo.dt;
+		sph_system.adjustPositionIntoRootDomain(dinfo);
+		dinfo.decomposeDomainAll(sph_system);
+		sph_system.exchangeParticle(dinfo);
+		PROBLEM::setEoS(sph_system);
+
+		for(short int loop = 0 ; loop <= PARAM::NUMBER_OF_DENSITY_SMOOTHING_LENGTH_LOOP ; ++ loop){
+			dens_tree.calcForceAllAndWriteBack(PTCL::CalcDensity(), sph_system, dinfo);
+		}
+		PTCL::CalcPressure(sph_system);
+		drvt_tree.calcForceAllAndWriteBack(PTCL::CalcDerivative(), sph_system, dinfo);
+		hydr_tree.calcForceAllAndWriteBack(PTCL::CalcHydroForce(), sph_system, dinfo);
+		#ifdef SELF_GRAVITY
+		grav_tree.calcForceAllAndWriteBack(PTCL::CalcGravityForce<PTCL::EPJ::Grav>(), PTCL::CalcGravityForce<PS::SPJMonopole>(), sph_system, dinfo);
+		#endif
+		PROBLEM::addExternalForce(sph_system, sysinfo);
+		#pragma omp parallel for
+		for(int i = 0 ; i < sph_system.getNumberOfParticleLocal() ; ++ i){
+			sph_system[i].finalKick(sysinfo.dt);
+		}
+		PROBLEM::postTimestepProcess(sph_system, sysinfo);
+		sysinfo.dt = getTimeStepGlobal<PTCL::RealPtcl>(sph_system);
+		OutputFileWithTimeInterval<PTCL::RealPtcl>(sph_system, sysinfo, PROBLEM::END_TIME);
+		++ sysinfo.step;
+		if(PS::Comm::getRank() == 0){
+			std::cout << "//================================" << std::endl;
+			std::cout << std::scientific << std::setprecision(16) << "time = " << sysinfo.time << ", dt = " << sysinfo.dt << std::endl;
+			std::cout << "step = " << sysinfo.step << std::endl;
+			std::cout << "//================================" << std::endl;
+		}
+		if(sysinfo.step % 30 == 0){
+			OutputBinary(sph_system, sysinfo);
+		}
+	}
+
+	PS::Finalize();
+	return 0;
+}
+
