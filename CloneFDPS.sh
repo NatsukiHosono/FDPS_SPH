fbb83fb9d5e5ad964a3dc7754eb504b091b718c8
Tyler LaBree <tlabree@bluehive.bluehive.circ.private>
2019-07-10 16:06:06 -0400

    Fixed CloneFDPS.sh

--- CloneFDPS.sh
+++ CloneFDPS.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 git clone https://github.com/FDPS/FDPS.git
+cd FDPS
 
 # This checks out a version of FDPS that is known to work with FDPS_SPH
 # Modify if you want to use a different FDPS version
 git checkout v5.0c
+cd ..
