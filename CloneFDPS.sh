80db6058e8120b1f43ea0e05bdd467033c38c843
Rene Gassmoeller <rene.gassmoeller@mailbox.org>
2019-07-10 08:56:57 -0700

    Add comment

--- CloneFDPS.sh
+++ CloneFDPS.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 git clone https://github.com/FDPS/FDPS.git
+
+# This checks out a version of FDPS that is known to work with FDPS_SPH
+# Modify if you want to use a different FDPS version
 git checkout v5.0c
