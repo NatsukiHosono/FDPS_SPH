0373058887e72f33712c9bcb1ca12dd59c9e5620
ScottHull <scottdhull21@gmail.com>
2020-08-20 16:08:36 -0400

    Experimental debug tools.

--- src/EoS.h
+++ src/EoS.h
@@ -337,6 +337,6 @@ static const EoS::IdealGas<PS::F64> Monoatomic(5. / 3.);
 static const EoS::IdealGas<PS::F64> Diatomic(1.4);
 static const EoS::Tillotson<PS::F64> Granite(2680.0, 16.0e+6, 3.5e+6, 18.00e+6, 18.0e+9, 18.0e+9, 0.5, 1.3, 5.0, 5.0);
 //static const EoS::Tillotson<PS::F64> Iron(7800.0,  9.5e+6, 2.4e+6 , 8.67e+6, 128.0e+9, 105.0e+9, 0.5, 1.5, 5.0, 5.0);
-static const EoS::ANEOS<PS::F64> Iron("eos/ironS2.rho_u.txt");
+static const EoS::ANEOS<PS::F64> Iron("eos/ironC.rho_u.txt");
 static const EoS::ANEOS<PS::F64> AGranite("eos/granite.rho_u.txt");
 static const EoS::ANEOS<PS::F64> ADunite("eos/duniteS2.rho_u.txt");
