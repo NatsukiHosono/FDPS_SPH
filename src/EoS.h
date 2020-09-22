8ea83cabdaadc3e3a212d54277c1f98f1fde699f
ScottHull <scottdhull21@gmail.com>
2020-09-22 12:40:31 -0400

    Fixed constant energy in old tillotson to not affect mode 2.

--- src/EoS.h
+++ src/EoS.h
@@ -4,6 +4,11 @@
 #include "interpolation.h"
 #include "parse.h"
 
+class TillotsonConstantInternalEnergy {
+public:
+    PS::F64 constant_internal_energy;
+};
+
 namespace EoS {
     //////////////////
     //abstract class
@@ -355,10 +360,15 @@ namespace EoS {
 PS::F64 getConstantEnergy(ParameterFile &parameter_file) {
     PS::F64 UnitMass = parameter_file.getValueOf("UnitMass", 6.0e+24);
     PS::F64 UnitRadi = parameter_file.getValueOf("UnitRadi", 6400e+3);
-    const PS::F64 tarMass = UnitMass;
-    const PS::F64 tarRadi = UnitRadi;
-    const PS::F64 Grav = 6.67e-11;
-    return 0.1 * Grav * tarMass / tarRadi;  // constant energy value
+    int mode = parameter_file.getValueOf("mode", 2);
+    if (mode == 1) {
+        const PS::F64 tarMass = UnitMass;
+        const PS::F64 tarRadi = UnitRadi;
+        const PS::F64 Grav = 6.67e-11;
+        return 0.1 * Grav * tarMass / tarRadi;  // constant energy value
+    } else {
+        return 0.0;
+    }
 }
 
 std::string input_file_eos("input.txt");
