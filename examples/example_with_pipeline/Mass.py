0bfa9070d7a958d88bd3c361f96d611d6f913ba2
chaualice <144171672+chaualice@users.noreply.github.com>
2026-07-24 14:33:11 -0400

    Add files via upload

---
+++ examples/example_with_pipeline/Mass.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np
+import scipy.constants
+import os as os
+
+filename = 'Mars'
+pathinput = '../../output/Analysis/'
+
+gi=pd.read_csv(pathinput+filename+'_identified_new.dat',sep='\t')
+
+Rp = 4e6
+disk=gi.loc[gi['tag2']==1]
+escaping=gi.loc[gi['tag2']==2]
+fullplanet = gi.loc[gi['tag2']==0]
+planet = fullplanet.loc[fullplanet['r'] < Rp]
+
+m = fullplanet['m'].sum()
+md = disk['m'].sum()
+ME =  5.9722e24
+
+omega = ((planet['jx'].sum()**2+planet['jy'].sum()**2+planet['jz'].sum()**2))**0.5/(planet['x']**2+planet['y']**2).sum()
+
+print("Masses of planet and disk: [ME]", m/ME, md/ME)
+print("Omega, rotation period [h]: ", omega, 2*np.pi/omega/(3600))
